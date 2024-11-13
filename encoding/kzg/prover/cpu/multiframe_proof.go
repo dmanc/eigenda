7b78a9020bdd2595496fb45bbd6dbee4da60a670
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2024-11-13 02:25:37 +0000

    refactor to add parametrized rs encoder

--- encoding/kzg/prover/cpu/multiframe_proof.go
+++ encoding/kzg/prover/cpu/multiframe_proof.go
@@ -19,47 +19,10 @@ type KzgCpuProofDevice struct {
 	Fs         *fft.FFTSettings
 	FFTPointsT [][]bn254.G1Affine // transpose of FFTPoints
 	SFs        *fft.FFTSettings
-	Srs        *kzg.SRS
-	G2Trailing []bn254.G2Affine
 }
 
 type WorkerResult struct {
 	err error
-}
-
-func (p *KzgCpuProofDevice) ComputeLengthProof(coeffs []fr.Element) (*bn254.G2Affine, error) {
-	inputLength := uint64(len(coeffs))
-	shiftedSecret := p.G2Trailing[p.KzgConfig.SRSNumberToLoad-inputLength:]
-	config := ecc.MultiExpConfig{}
-	//The proof of low degree is commitment of the polynomial shifted to the largest srs degree
-	var lengthProof bn254.G2Affine
-	_, err := lengthProof.MultiExp(shiftedSecret, coeffs, config)
-	if err != nil {
-		return nil, err
-	}
-	return &lengthProof, nil
-}
-
-func (p *KzgCpuProofDevice) ComputeCommitment(coeffs []fr.Element) (*bn254.G1Affine, error) {
-	// compute commit for the full poly
-	config := ecc.MultiExpConfig{}
-	var commitment bn254.G1Affine
-	_, err := commitment.MultiExp(p.Srs.G1[:len(coeffs)], coeffs, config)
-	if err != nil {
-		return nil, err
-	}
-	return &commitment, nil
-}
-
-func (p *KzgCpuProofDevice) ComputeLengthCommitment(coeffs []fr.Element) (*bn254.G2Affine, error) {
-	config := ecc.MultiExpConfig{}
-
-	var lengthCommitment bn254.G2Affine
-	_, err := lengthCommitment.MultiExp(p.Srs.G2[:len(coeffs)], coeffs, config)
-	if err != nil {
-		return nil, err
-	}
-	return &lengthCommitment, nil
 }
 
 func (p *KzgCpuProofDevice) ComputeMultiFrameProof(polyFr []fr.Element, numChunks, chunkLen, numWorker uint64) ([]bn254.G1Affine, error) {
